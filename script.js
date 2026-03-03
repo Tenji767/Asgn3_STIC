2fefbc0b3ba082a91f0895320f5ba36e100d5a71
Tenji767 <144552671+Tenji767@users.noreply.github.com>
2026-03-03 11:38:50 -0500

    i forgot what i did but it was something

--- script.js
+++ script.js
@@ -5,14 +5,15 @@ let city = submit.value;
 
 submit.addEventListener("keyup", async ()=> {
 
-    const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit={limit}\&appid=${locationapikey}`);
+    const responseLocation = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${locationapikey}`);
     
-    const data = await response.json();
+    const data = await responseLocation.json();
 
     const lat = data[0].lat;
     const lon = data[0].lon;
 
-    
+    const responseWeather = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&hourly=temperature_2m,relative_humidity_2m,precipitation_probability,precipitation,visibility,weather_code`);
+
 
 }
 
