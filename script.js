26682470e498f77302256c8187db8a62b028d26e
Tenji767 <144552671+Tenji767@users.noreply.github.com>
2026-03-03 09:29:48 -0500

    i added the first api call

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-const submit=document.getElementById("weather-input");\
-const locationapikey = "
+const submit=document.getElementById("weather-input");
+const locationapikey = "01212a7cb6a8db7638a95a05edfb6a86";
 
-submit.addEventListener("keyup", asyc ()=>{
-  
+let city = submit.value;
+
+submit.addEventListener("keyup", async ()=> {
+
+    const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit={limit}\&appid=${locationapikey}`);
+    
+    const data = await response.json();
+
+    const lat = data[0].lat;
+    const lon = data[0].lon;
+
+    
+
 }
+
+);
