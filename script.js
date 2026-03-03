ac0e9adaa5729c7092993f298af3b1f21039cc1b
Juhwan <wnghks3357@gmail.com>
2026-03-03 11:54:38 -0500

    add button

--- script.js
+++ script.js
@@ -1,19 +1,15 @@
-const submit=document.getElementById("weather-input");
-const locationapikey = "01212a7cb6a8db7638a95a05edfb6a86";
+const submit = document.getElementById('locationinput');
+const locationapikey = '01212a7cb6a8db7638a95a05edfb6a86';
 
 let city = submit.value;
 
-submit.addEventListener("keyup", async ()=> {
+submit.addEventListener('keyup', async () => {
+  const response = await fetch(
+    `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit={limit}\&appid=${locationapikey}`
+  );
 
-    const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit={limit}\&appid=${locationapikey}`);
-    
-    const data = await response.json();
+  const data = await response.json();
 
-    const lat = data[0].lat;
-    const lon = data[0].lon;
-
-    
-
-}
-
-);
+  const lat = data[0].lat;
+  const lon = data[0].lon;
+});
