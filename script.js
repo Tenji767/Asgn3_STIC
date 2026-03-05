fceb05c09d347f2d28f0751fb96b0eb4dcfa337a
Tenji767 <144552671+Tenji767@users.noreply.github.com>
2026-03-04 20:05:50 -0500

    Refactor location input handling and fetch logic

--- script.js
+++ script.js
@@ -1,31 +1,45 @@
-const submit = document.getElementById('locationinput');
+const submit = document.getElementById('submitLocation');
+const inputcity = document.getElementById('locationinput');
 const locationapikey = '01212a7cb6a8db7638a95a05edfb6a86';
 
-let city = submit.value;
+let city = inputcity.value;
 
-submit.addEventListener('keyup', async () => {
-  const response = await fetch(
-    `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit={limit}\&appid=${locationapikey}`
-  );
+submit.addEventListener('click', async () => {
 
-<<<<<<< HEAD
-  const data = await response.json();
-
-  const lat = data[0].lat;
-  const lon = data[0].lon;
-});
-=======
-    const responseLocation = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${locationapikey}`);
+    let responseLocation = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${locationapikey}`);
     
-    const data = await responseLocation.json();
+    let data = await responseLocation.json();
 
-    const lat = data[0].lat;
-    const lon = data[0].lon;
+    if (data.length ===0) {
+  
+    }
+  
+    let lat = data[0].lat;
+    let lon = data[0].lon;
 
-    const responseWeather = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&hourly=temperature_2m,relative_humidity_2m,precipitation_probability,precipitation,visibility,weather_code`);
+    let responseWeather = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&hourly=temperature_2m,relative_humidity_2m,precipitation_probability,precipitation,visibility,weather_code`);
 
+    let data2 = await responseWeather.json();
+
+    data2.hourly.time.forEach((time,index) => {
+      
+      const temp = data2.hourly.temperature_2m[index];
+      const humidity = data2.hourly.relative_humidity_2m[index];
+      const precipProb = data2.hourly.precipitation_probability[index];
+      const precipAmt = data2.hourly.precipitation[index];//in mm
+      const weatherCode = data2.hourly.weather_code[index];//check to see what each code corresponds to, make it the background
+
+      const weatherCard = document.createElement('div');
+      weatherCard.innerHTML = 
+        `<h3>${time}</h3>
+          <h2>${temp}</h2>
+          <h4>${precibProb}%</h4>
+        `
+        
+      
+    });
+  
 
 }
 
 );
->>>>>>> 2fefbc0b3ba082a91f0895320f5ba36e100d5a71
